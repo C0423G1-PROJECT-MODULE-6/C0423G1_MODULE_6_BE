a3525815f7c84e87cddbd7b617a6734762d18d52
damthoaitin1004 <damthoaitin100041997@gmail.com>
2023-10-12 14:43:44 +0700

    [TinDT][Fix Cart]

--- src/main/java/com/example/c4zone/model/order/Cart.java
+++ src/main/java/com/example/c4zone/model/order/Cart.java
@@ -1,7 +1,7 @@
 package com.example.c4zone.model.order;
 
 import com.example.c4zone.model.product.Product;
-import com.example.c4zone.model.user.User;
+import com.example.c4zone.model.user.AppUser;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -21,7 +21,7 @@ public class Cart {
 
     @ManyToOne
     @JoinColumn(name = "id_user",referencedColumnName = "id")
-    private User user;
+    private AppUser user;
 
 
     @ManyToOne
