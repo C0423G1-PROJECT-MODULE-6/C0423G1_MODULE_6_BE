f2acdb15c12da9134f3a2c2ba26a5e179935a793
la2305 <lequangphuoc2305@gmail.com>
2023-10-12 10:37:44 +0700

    [phuoc][updateemployee]

--- src/main/java/com/example/c4zone/dto/user/employee/EmployeeDto.java
+++ src/main/java/com/example/c4zone/dto/user/employee/EmployeeDto.java
@@ -4,18 +4,20 @@ import com.example.c4zone.model.user.Role;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 
+import java.sql.Date;
+
 public class EmployeeDto implements Validator {
     private Long id;
     private String userName;
     private Role role;
-    private String name;
+    private String employeeName;
     private String employeeCode;
     private String employeeAddress;
     private String employeePhone;
     private String employeeImage;
     private String employeeIdCard;
-    private String employeeBirthday;
-    private String employeeStartDate;
+    private Date employeeBirthday;
+    private Date employeeStartDate;
     private String email;
     private String employeeGender;
     private boolean flagDelete;
