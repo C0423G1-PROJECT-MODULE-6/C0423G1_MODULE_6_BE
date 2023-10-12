3d71053a02ed965d8cfdfb01fcf43c29eb22f2b3
hongnguyen0711 <nguyenhongnguyen40@gmail.com>
2023-10-12 09:35:07 +0700

    [NguyenNH][Customer][Display list customer]

--- src/main/java/com/example/c4zone/repository/customer/ICustomerRepository.java
+++ src/main/java/com/example/c4zone/repository/customer/ICustomerRepository.java
@@ -1,8 +1,21 @@
 package com.example.c4zone.repository.customer;
 
+import com.example.c4zone.model.customer.Customer;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 
-@Controller
 
-public interface ICustomerRepository {
+
+public interface ICustomerRepository extends JpaRepository<Customer, Long> {
+    @Query(value = "select * from customer where name_customer like :name and TIMESTAMPDIFF(YEAR, birth_date_customer, CURDATE()) =:age and gender_customer = :gender ", nativeQuery = true)
+    Page<Customer> findAllCustomerByAge(Pageable pageable, @Param("name") String valueSearchName,@Param("age") String valueAge, @Param("gender") Boolean valueSearchGender);
+
+    @Query(value = "select * from customer where name_customer like :name and TIMESTAMPDIFF(YEAR, birth_date_customer, CURDATE()) >= 0 and gender_customer = :gender ", nativeQuery = true)
+    Page<Customer> findAllCustomerByGender(Pageable pageable, @Param("name") String valueSearchName, @Param("gender") Boolean valueSearchGender);
+    @Query(value = "select * from customer where name_customer like :name and TIMESTAMPDIFF(YEAR, birth_date_customer, CURDATE()) >= 0 ", nativeQuery = true)
+    Page<Customer> findAllCustomerByName(Pageable pageable, @Param("name") String valueSearchName);
 }
--- src/main/java/com/example/c4zone/service/customer/ICustomerService.java
+++ src/main/java/com/example/c4zone/service/customer/ICustomerService.java
@@ -1,4 +1,9 @@
 package com.example.c4zone.service.customer;
 
+import com.example.c4zone.model.customer.Customer;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 public interface ICustomerService {
+    Page<Customer> findCustomerByNameAndAge(Pageable pageable, String valueSearchName, String valueSearchAge, Boolean valueSearchGender);
 }
