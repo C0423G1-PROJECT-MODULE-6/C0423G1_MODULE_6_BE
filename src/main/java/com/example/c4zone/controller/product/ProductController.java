d5a26c32bc66fc5dc1cd0338742b441b28a3ae3e
dinhquan2007 <dinhquan2007@gmail.com>
2023-10-17 09:43:35 +0700

    [C04 Zone][Product][QuanND] Chinh lai controller

--- src/main/java/com/example/c4zone/controller/product/ProductController.java
+++ src/main/java/com/example/c4zone/controller/product/ProductController.java
@@ -152,9 +152,6 @@ public class ProductController {
             @RequestParam(value = "value", required = false, defaultValue = "") String value) {
         Page<IProductDto> productDtoPage;
         Pageable pageable;
-        if (page < 0) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
         switch (sort) {
             case "name":
                 pageable = PageRequest.of(page, 5, Sort.by("name").ascending());
@@ -174,12 +171,8 @@ public class ProductController {
         }
         switch (choose) {
             case "name":
-                if (value.matches("^\\w+$")) {
                     productDtoPage = productService.getAllByName(pageable, value);
                     break;
-                } else {
-                    return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-                }
             case "price":
                 productDtoPage = productService.getAllByPrice(pageable, value);
                 break;
